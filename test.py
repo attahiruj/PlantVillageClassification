62fb765be856236c607db00fe78e80f440bd4748
attahiruj <attahiruj@gmail.com>
2024-04-08 22:14:11 +0100

    - working api calls - test data set from internet

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import requests
 
 # Read the image file
 url = 'http://127.0.0.1:5000/predict'
-img_path = 'data/plantvillage/Tomato___Septoria_leaf_spot/01f54ad9-9c03-4ffd-86f4-829fc2939120___Matt.S_CG 0702.JPG'
+img_path = 'data/test/9.jpeg'
 
 resp = requests.post(
                         "http://localhost:5000/predict",
